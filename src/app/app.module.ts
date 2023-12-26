eb04212125a08cf0e2229c3fc7a9b657e4d80384
Matt Payne <Payne@MattPayne.org>
2023-12-26 12:25:03 -0600

    Runs but has layout problems - hamburger menu is not down the left rail, but in the upper left corner.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { Route1Component } from './route1/route1.component';
+import { Route2Component } from './route2/route2.component';
+import { Route3Component } from './route3/route3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Route1Component,
+    Route2Component,
+    Route3Component
   ],
   imports: [
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
---
+++ src/app/route3/route3.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-route3',
+  templateUrl: './route3.component.html',
+  styleUrls: ['./route3.component.scss']
+})
+export class Route3Component {
+
+}
