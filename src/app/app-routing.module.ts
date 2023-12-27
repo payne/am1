9e0bdf8db30908d99b66f8e363286d8b57e86aea
Matt Payne <Payne@MattPayne.org>
2023-12-26 20:11:59 -0600

    About component links to github.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home/home.component';
 import { Route1Component } from './route1/route1.component';
 import { Route2Component } from './route2/route2.component';
 import { Route3Component } from './route3/route3.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'route1', component: Route1Component },
   { path: 'route2', component: Route2Component },
   { path: 'route3', component: Route3Component },
+  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
